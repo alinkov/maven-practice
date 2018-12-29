d00a030666e1ae29e1f1edd64b35ef788a98af69
Andrey Nikishin <polarik@ya.ru>
2018-12-29 08:20:25 +0300

    Adding scan all for default

--- src/main/java/ru/hh/school/NamingMojo.java
+++ src/main/java/ru/hh/school/NamingMojo.java
@@ -12,8 +12,16 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 import org.apache.maven.project.MavenProject;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.*;
+import java.util.Iterator;
+import java.util.Vector;
+
 /**
- * Goal which changes a greeted name. Default class - Target, default Field - Field.
+ * Goal which changes a greeted name.
  */
 
 @Mojo(name = "setname", defaultPhase = LifecyclePhase.PROCESS_CLASSES, threadSafe = true)
@@ -22,15 +30,15 @@ public class NamingMojo extends AbstractMojo {
     private String groupTarget;
 
     /**
-     * Target class.
+     * Target class. Default scan all.
      */
-    @Parameter(defaultValue = "Target", property = "classTarget")
+    @Parameter(defaultValue = "", property = "classTarget")
     private String classTarget;
 
     /**
-     * Target field.
+     * Target field. Default scan all.
      */
-    @Parameter(defaultValue = "Field", property = "fieldTarget")
+    @Parameter(defaultValue = "", property = "fieldTarget")
     private String fieldTarget;
 
     /**
@@ -45,17 +53,72 @@ public class NamingMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException {
         getLog().info("Замена полей");
 
+
         ClassPool pool = ClassPool.getDefault();
         try {
+            Vector<String> classList = new Vector<String>();
             for( String cp : project.getCompileClasspathElements()) {
                 pool.insertClassPath(cp);
+                if (classTarget==null || classTarget.equals("")) {
+                    processFilesFromFolder(new File(cp),"", classList);
+                }
             }
-            CtClass ct = pool.get(groupTarget+classTarget);
-            CtField field = ct.getField(fieldTarget);
-            getLog().info(String.format("%s %s", message, field.getConstantValue()));
+            if (classTarget!=null && classTarget.length()!=0) {
+                classList.add(groupTarget.concat(classTarget));
+            }
+            for (String className : classList) {
+                try {
+                    CtClass ct = pool.get(className);
+                    if (fieldTarget == null || fieldTarget.equals("")) {
+                        CtField[] fields = ct.getFields();
+                        for (CtField field : fields) {
+                            getLog().info(String.format("%s %s#%s: %s", message, className, field.getName(), field.getConstantValue()));
+                        }
+                    } else {
+                        try {
+                            CtField field = ct.getField(fieldTarget);
+                            getLog().info(String.format("%s %s#%s: %s", message, className, fieldTarget, field.getConstantValue()));
+                        }
+                        catch (NotFoundException e) {
+                            if (classList.size()<2) {
+                                getLog().error(e.getMessage());
+                                throw new MojoExecutionException(e.getMessage());
+                            }
+                        }
+                    }
+                }
+                catch (NotFoundException e) {
+                    getLog().error(e.getMessage());
+                    throw new MojoExecutionException(e.getMessage());
+                }
+            }
         } catch (NotFoundException | DependencyResolutionRequiredException e) {
             getLog().error(e.getMessage());
             throw new MojoExecutionException(e.getMessage());
+        }
+    }
+
+    private void processFilesFromFolder(File folder, String packageName, Vector<String> classList)
+    {
+        File[] folderEntries = folder.listFiles();
+        for (File entry : folderEntries)
+        {
+            if (entry.isDirectory())
+            {
+                String newPackageName;
+                if (packageName==null || packageName.equals("")) {
+                    newPackageName = new String(entry.getName());
+                }
+                else {
+                    newPackageName = packageName.concat(".").concat(entry.getName());
+                }
+                processFilesFromFolder(entry, newPackageName, classList);
+                continue;
+            }
+            String fileName = entry.getName();
+            if (fileName.endsWith(".class")) {
+                classList.add(packageName.concat(".").concat(fileName.substring(0,fileName.length()-6)));
+            }
         }
     }
 }
