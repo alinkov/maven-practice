9702574927ad183008d46eba5d8c21df2b235495
Kseniya Kolesnikova <hyper1995@gmail.com>
2019-01-01 22:04:51 +0300

    remade getCount method

--- check-files-maven-plugin/src/main/java/ru/hh/school/CheckFilesMojo.java
+++ check-files-maven-plugin/src/main/java/ru/hh/school/CheckFilesMojo.java
@@ -3,6 +3,9 @@ package ru.hh.school;
 import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
@@ -22,52 +25,61 @@ public class CheckFilesMojo extends AbstractMojo {
     /**
      * Number to compare with count files in directory
      */
-    @Parameter(property = "count", defaultValue = "22")
+    @Parameter(property = "count", defaultValue = "10")
     private int count;
+
+    private int countF;
 
     public void execute()
             throws MojoExecutionException {
-        int countFiles = getCount(path);
+        if (path.exists()) {
+            countF = 0;
+            List<String> detailing = new ArrayList<String>();
+            getCount(path, detailing);
+            printResult(detailing);
+        }
+        else{
+            getLog().error("Directory " + path + " does not exists.");
+        }
+    }
+
+    private void getCount(File directory, List<String> detailing) {
+        int filesCount = 0;
+        int directoryCount = 0;
+        if (!directory.canRead()) {
+            getLog().error("Directory " + directory + " cannot be read");
+            return;
+        }
+        File[] listFiles = directory.listFiles();
+        if (listFiles != null) {
+            for (File subFile : listFiles) {
+                if (subFile.isFile()) {
+                    filesCount++;
+                } else {
+                    directoryCount++;
+                    getCount(subFile, detailing);
+                }
+            }
+        }
+        countF+=filesCount;
+        if (countF<count) {
+            detailing.add(String.format("Directory %s contains %s subdirectories and %s files.", directory, directoryCount, filesCount));
+        }
+    }
+
+    private void printResult(List<String> detailing) {
         getLog().info("------------------------------------------------------------------------");
-        getLog().info("              The required directory has " + countFiles + " files.");
-        if (countFiles > count) {
+        getLog().info("              The required directory has " + countF + " files.");
+        if (countF > count) {
             getLog().info("                   That's more than " + count + " files.");
         }
         else {
             getLog().info("                   That's less than " + count + " files.");
             getLog().info("------------------------------------------------------------------------");
             getLog().info("Detailing:");
-            printDetailing(path);
-        }
-        getLog().info("------------------------------------------------------------------------");
-    }
-    private int getCount(File directory) {
-        int filesCount = 0;
-        File[] listFiles = directory.listFiles();
-        assert listFiles != null;
-        for (File subFile : listFiles) {
-            if (subFile.isFile()) {
-                filesCount++;
-            } else {
-                filesCount += getCount(subFile);
+            for(String detail : detailing) {
+                getLog().info(detail);
             }
         }
-        return (filesCount);
-    }
-
-    private void printDetailing(File directory) {
-        int filesCount = 0;
-        int directoryCount = 0;
-        File[] listFiles = directory.listFiles();
-        assert listFiles != null;
-        for (File subFile : listFiles) {
-            if (subFile.isFile()) {
-                filesCount++;
-            } else {
-                directoryCount++;
-                printDetailing(subFile);
-            }
-        }
-        getLog().info(String.format("Directory %s contains %s subdirectories and %s files.", directory, directoryCount, filesCount));
     }
 }
