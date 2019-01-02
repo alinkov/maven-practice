d190d20195999e8c5d4238fcb950cdf9dced4be5
Kseniya Kolesnikova <hyper1995@gmail.com>
2019-01-02 21:50:51 +0300

    added checkstyle-plugin

--- check-files-maven-plugin/src/main/java/ru/hh/school/CheckFilesMojo.java
+++ check-files-maven-plugin/src/main/java/ru/hh/school/CheckFilesMojo.java
@@ -10,39 +10,47 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 /**
- *   This plugin counts the files number in a directory
- *  and displays the detailing (count of сontaining files and subdirectories)
+ *  This plugin counts the files number in a directory.
+ *  and displays the detailing (count of containing files and subdirectories).
  *  if their number does not exceed the specified.
+ *  @author KseniaKolesnikova
  */
 @Mojo(name = "check-files")
 public class CheckFilesMojo extends AbstractMojo {
     /**
-     * Path to the top directory
+     * Path to the top directory.
      */
-    @Parameter(property = "path", defaultValue = "./")
+    @Parameter(property = "path", defaultValue = "${basedir}")
     private File path;
 
     /**
-     * Number to compare with count files in directory
+     * Number to compare with count files in the directory.
      */
     @Parameter(property = "count", defaultValue = "10")
     private int count;
 
-    private int countF;
+    /**
+     * Number of files in the directory.
+     */
+    private int fileCount;
 
-    public void execute()
-            throws MojoExecutionException {
+    @Override
+    public void execute() throws MojoExecutionException {
         if (path.exists()) {
-            countF = 0;
+            fileCount = 0;
             List<String> detailing = new ArrayList<String>();
             getCount(path, detailing);
             printResult(detailing);
-        }
-        else{
+        } else {
             getLog().error("Directory " + path + " does not exists.");
         }
     }
 
+    /**
+     * Recursive search files count.
+     * @param directory is path to the calculating directory
+     * @param detailing is list of strings for displaying detailing
+     */
     private void getCount(File directory, List<String> detailing) {
         int filesCount = 0;
         int directoryCount = 0;
@@ -61,25 +69,30 @@ public class CheckFilesMojo extends AbstractMojo {
                 }
             }
         }
-        countF+=filesCount;
-        if (countF<count) {
-            detailing.add(String.format("Directory %s contains %s subdirectories and %s files.", directory, directoryCount, filesCount));
+        fileCount += filesCount;
+        if (fileCount < count) {
+            detailing.add(String.format("Directory %s contains %s subdirectories and %s files.",
+                    directory, directoryCount, filesCount));
         }
     }
 
+    /**
+     * Displaying detailing.
+     * @param detailing is path to the calculating directory
+     */
     private void printResult(List<String> detailing) {
         getLog().info("------------------------------------------------------------------------");
-        getLog().info("              The required directory has " + countF + " files.");
-        if (countF > count) {
+        getLog().info("              The required directory has " + fileCount + " files.");
+        if (fileCount > count) {
             getLog().info("                   That's more than " + count + " files.");
-        }
-        else {
+        } else {
             getLog().info("                   That's less than " + count + " files.");
             getLog().info("------------------------------------------------------------------------");
             getLog().info("Detailing:");
-            for(String detail : detailing) {
+            for (String detail : detailing) {
                 getLog().info(detail);
             }
+            getLog().info("------------------------------------------------------------------------");
         }
     }
 }
