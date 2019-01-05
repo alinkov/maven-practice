1e45140d3081fb0ac1fccdd5913932403746c482
Artem Filippoff <filippoff@mail.ru>
2019-01-06 01:36:35 +0300

    Edit after comments on github

--- homework-maven-plugin/src/main/java/hh/TestFiles.java
+++ homework-maven-plugin/src/main/java/hh/TestFiles.java
@@ -8,65 +8,63 @@ import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
 import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-
 import java.util.ArrayList;
 
 /**
  * Goal which scan dir and check files
  */
-@Mojo( name = "scanAndTest", defaultPhase = LifecyclePhase.PROCESS_SOURCES )
-public class TestFiles
-    extends AbstractMojo
-{
+@Mojo(name = "scanAndTest", defaultPhase = LifecyclePhase.PROCESS_SOURCES)
+public class TestFiles extends AbstractMojo {
 
-    private void recursiveDirScan(File start, ArrayList<File> filelist)
-    {
-        if(start.isDirectory())
-        {
-            for(File item : start.listFiles()){
-                if(item.isDirectory()){
-                    recursiveDirScan(item, filelist);
-                }
-                else{
-                    filelist.add(item);
+    private void recursiveDirScan(File start, ArrayList<File> filelist) {
+        try {
+            if (start.isDirectory()) {
+                for (File item : start.listFiles()) {
+                    if (item.isDirectory()) {
+                        recursiveDirScan(item, filelist);
+                    } else {
+                        filelist.add(item);
+                    }
                 }
             }
+        } catch (Exception e) {
+            /* Не могу придумать, что тут правильно будет делать. Нет доступа, значит просто пропустить? */
         }
     }
 
     /**
      * Location of the file.
      */
-    @Parameter( defaultValue = "${project.build.directory}", property = "scanDir", required = true )
+    @Parameter(defaultValue = "${project.build.directory}", property = "scanDir", required = true)
     private File scanDirectory;
 
-    @Parameter( defaultValue = "5", property = "minFileCount", required = true )
-    private String minFileCount;
+    /**
+     * Minimum of files count
+     */
+    @Parameter(defaultValue = "0", property = "minFileCount", required = true)
+    private long minFileCount;
 
-    @Parameter( defaultValue = "100000", property = "maxFilesSize", required = true )
-    private String maxFilesSize;
+    /**
+     * Maximum size of files
+     */
+    @Parameter(defaultValue = "100000", property = "maxFilesSize", required = true)
+    private long maxFilesSize;
 
-    public void execute()
-        throws MojoExecutionException
-    {
+    public void execute() throws MojoExecutionException {
         File f = scanDirectory;
         ArrayList<File> filelist = new ArrayList<File>();
-        recursiveDirScan(f,filelist);
+        recursiveDirScan(f, filelist);
 
-        Long minCount = new Long(Integer.parseInt(minFileCount));
-        Long maxSize = new Long(Integer.parseInt(maxFilesSize));
+        System.out.println(scanDirectory);
 
-        if (filelist.size() < minCount)
-            throw new MojoExecutionException( "In direcrory must be at least "+minFileCount+" files" );
+        if (filelist.size() < minFileCount)
+            throw new MojoExecutionException("In direcrory must be at least " + minFileCount + " files");
 
-        Long fileSize = new Long(0);
-        for (File item : filelist)
-        {
+        long fileSize = 0;
+        for (File item : filelist) {
             fileSize += item.length();
-            if(fileSize > maxSize)
-                throw new MojoExecutionException( "Maximum size of files in direcrory ("+maxSize+") exceeded" );
+            if (fileSize > maxFilesSize)
+                throw new MojoExecutionException("Maximum size of files in direcrory (" + maxFilesSize + ") exceeded");
         }
     }
 }
